01159f25ef588c6bf5a134043d58626d6a5852f2
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-13 12:49:11 +0600

    dummy todos passing

---
+++ src/components/Home.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Todos from './Todos';
+
+export default function Home() {
+
+  const dummyTodos=[
+
+    {
+
+        id: 1,
+        title: 'todo title 1',
+        desc: 'todo1 description is here...'
+
+
+
+    },
+
+    {
+
+        id: 2,
+        title: 'todo title 2',
+        desc: 'todo2 description is here...'
+
+
+
+    }
+
+
+
+
+  ];
+
+
+  return (
+    <div>
+
+        <Todos todos={dummyTodos}/>
+
+      
+    </div>
+  )
+}
