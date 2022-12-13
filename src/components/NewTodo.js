a3b87f34265ddaa72b99073bf60bcbcc205ab2be
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-13 20:24:56 +0600

    usestate for handle form

--- src/components/NewTodo.js
+++ src/components/NewTodo.js
@@ -1,16 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import style from './newtodo.module.css';
 
 export default function NewTodo() {
+
+  const [todo,setTodo]=useState({title:"",desc:""});
+
+  const {title,desc}=todo;
+
+  const onHandleSubmit=(e)=>{
+
+    e.preventDefault();
+    console.log(todo);
+
+    setTodo({title:"",desc:""});
+
+  }
+  
+  const onHandleChange=(e)=>{
+
+    const name=e.target.name;
+
+    setTodo((oldTodo)=>{
+
+      return {...oldTodo,[name]:e.target.value}
+
+    });
+
+  }
+
+
+
+
   return (
     <div>
-      <form className={style.form}>
+      <form className={style.form} onSubmit={onHandleSubmit}>
 
             <div className={style.formfield}>
 
               <label htmlFor='title'>Title</label>
-              <input type='text' id='title' name='title'/>
+              <input type='text' id='title' name='title' value={title} onChange={onHandleChange}/>
 
 
             </div>
@@ -18,7 +47,7 @@ export default function NewTodo() {
             <div className={style.formfield}>
 
               <label htmlFor='desc'>Description</label>
-              <textarea type='text' id='desc' name='desc'/>
+              <textarea type='text' id='desc' name='desc' value={desc} onChange={onHandleChange}/>
 
 
             </div>
